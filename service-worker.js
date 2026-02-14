315e2c143f3876b75eaade28aaa92d4027a58b2d
daylian <50170310+daylian@users.noreply.github.com>
2026-02-14 17:04:41 -0500

    Update cache URLs in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'pilot-logbook-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
+  '/Pilot-logbook/',
+  '/Pilot-logbook/index.html',
   'https://unpkg.com/react@18/umd/react.production.min.js',
   'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
   'https://unpkg.com/@babel/standalone/babel.min.js',
